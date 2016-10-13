59b255594493de97c3e1cfa1f32a9b873539d613
Anthony Whitaker <AnthonyWhitakerJr@gmail.com>
2016-10-12 21:26:59 -0400

    Begin UI Design

---
+++ WarframeReliquary/FissureRelicCell.swift
@@ -0,0 +1,18 @@
+//
+//  FissureRelicCell.swift
+//  WarframeReliquary
+//
+//  Created by Anthony Whitaker on 10/12/16.
+//  Copyright © 2016 Anthony Whitaker. All rights reserved.
+//
+
+import UIKit
+
+class FissureRelicCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
